98534d1e5a0165f1cd167177a7e0ba2ce6c4b1b1
MSciegata <MSciegata@sofrecom.com.ar>
2017-11-08 12:38:42 -0300

    Corrección head + scripts

--- style.php
+++ style.php
@@ -1,13 +1,18 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>YPF Style | Components</title>
-	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/font-awesome.css">
-	<link rel="stylesheet" href="css/styles.css">
-</head>
+<html lang="es">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>YPF Style | Components</title>
+		<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+	    <link rel="stylesheet" href="css/datepicker.css">
+	    <link rel="stylesheet" href="css/timepicker.css">
+	    <link rel="stylesheet" href="css/select-search.min.css">
+	    <link rel="stylesheet" href="css/font-awesome.css">
+	    <link rel="stylesheet" href="js/plugins/jquery-ui/jquery-ui.min.css">
+	    <link rel="stylesheet" href="css/ui.jqgrid.min.css">
+	    <link rel="stylesheet" href="css/styles.css">
+	</head>
 <body>
 	<?php include 'header.php';?>
 	<main>
@@ -342,14 +347,63 @@
 		</div>
 	</main>
 	<?php include 'footer.php';?>
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
-	<script>
-		$('#laytime-tab a').click(function (e) {
-		  e.preventDefault()
-		  $(this).tab('show')
-		})
-	</script>
+	<!-- Scripts -->
+        <script src="js/jquery-2.2.4.min.js"></script>
+        <script src="js/plugins/jquery-ui/jquery-ui.min.js"></script>
+        <script src="js/datepicker.min.js"></script>
+        <script src="js/datepicker.es.js"></script>
+        <script src="js/timepicker.js"></script>
+        <script src="js/select-search.min.js"></script>
+        <script src="js/autosize.min.js"></script>
+
+        <!-- JQ Grid -->
+        <script src="js/plugins/jqgrid/jquery.jqgrid.min.js"></script>
+        <script src="js/plugins/jqgrid/grid.locale-es.js"></script>
+        <script src="js/listado_viajes-SPOT.js"></script>
+
+        <!-- Bootstrap -->
+        <script src="bootstrap/js/bootstrap.min.js"></script>
+		<script>
+			$(function () {
+                            
+                // TextAreas con autoresize        
+                autosize($('textarea'));
+
+				// Tab
+				$('#laytime-tab a').click(function (e) {
+				  e.preventDefault();
+				  $(this).tab('show');
+				});
+
+				// Datepicker
+				$('.input-date').datepicker();
+
+				// Timepicker
+				$(".timepicker").click(function () {
+                    $(this).timepicker('showWidget');
+                });
+
+				//Select with search 
+                $(".select-buscador").select2();
+
+                $(".select-buscador").each(function (i) {
+                    if ($(this).val() === "") {
+                        $(this).parent('.input-group').find('.select2-selection__rendered').addClass('texto-blanco');
+                    }
+                });
+
+                $(".select-buscador").change(function () {
+                    if ($(this).val() !== "") {
+                        $(this).parent('.input-group').find('label').addClass('label-arriba').removeClass('label-abajo');
+                        $(this).parent('.input-group').find('.select2-selection__rendered').removeClass('texto-blanco');
+                    } else {
+                        $(this).parent('.input-group').find('label').addClass('label-abajo').removeClass('label-arriba');
+                        $(this).parent('.input-group').find('.select2-selection__rendered').addClass('texto-blanco');
+                    }
+                    ;
+                });
+
+            });
+		</script>
 </body>
 </html>
