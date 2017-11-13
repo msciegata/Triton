3ba0334839734b93f1e6675236e11d86c66dae50
SOFRECOM\FLamata <ferlamata17@gmail.com>
2017-11-13 09:45:07 -0300

    Fine uploader estilo e incorporacion

--- registrar-necesidad.php
+++ registrar-necesidad.php
@@ -13,7 +13,95 @@
         <link rel="stylesheet" href="js/plugins/jquery-ui/jquery-ui.min.css">
         <link rel="stylesheet" href="css/ui.jqgrid.min.css">
         <link rel="stylesheet" href="css/styles.css">
-	</head>
+        
+        
+        <script src="fine-uploader/all.fine-uploader.js"></script>
+        <link rel="stylesheet" href="fine-uploader/fine-uploader-gallery.css">
+        <script type="text/template" id="qq-template-gallery">
+         <div class="qq-uploader-selector qq-gallery" qq-drop-area-text="">
+        <div class="qq-total-progress-bar-container-selector qq-total-progress-bar-container">
+            <div role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" class="qq-total-progress-bar-selector qq-progress-bar qq-total-progress-bar"></div>
+        </div>
+        <div class="qq-upload-drop-area-selector qq-upload-drop-area" qq-hide-dropzone>
+            <span class="qq-upload-drop-area-text-selector"></span>
+        </div>
+        <div class="input-group">
+            <label for="contrato-SAP" class="label-campo label-abajo">Adjuntar archivo</label>
+            <div class="qq-upload-button-selector well-add-button">
+                <a href="#">
+                    <span class="fa fa-plus well-add-button-fa"></span>
+                </a>
+            </div>
+        </div>
+        <span class="qq-drop-processing-selector qq-drop-processing">
+            <span>Processing dropped files...</span>
+            <span class="qq-drop-processing-spinner-selector qq-drop-processing-spinner"></span>
+        </span>
+        <ul class="qq-upload-list-selector qq-upload-list" role="region" aria-live="polite" aria-relevant="additions removals">
+            <li>
+                <span role="status" class="qq-upload-status-text-selector qq-upload-status-text"></span>
+                <div class="qq-progress-bar-container-selector qq-progress-bar-container">
+                    <div role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" class="qq-progress-bar-selector qq-progress-bar"></div>
+                </div>
+                <span class="qq-upload-spinner-selector qq-upload-spinner"></span>
+                <div class="qq-thumbnail-wrapper">
+                    <img class="qq-thumbnail-selector" qq-max-size="120" qq-server-scale>
+                </div>
+                <button type="button" class="qq-upload-cancel-selector qq-upload-cancel well-trash-button">
+                    <span class="fa fa-trash"></span>
+                </button>
+                <button type="button" class="qq-upload-retry-selector qq-upload-retry">
+                    <span class="qq-btn qq-retry-icon" aria-label="Retry"></span>
+                    Retry
+                </button>
+
+                <div class="qq-file-info">
+                    <div class="qq-file-name">
+                        <span class="qq-upload-file-selector qq-upload-file"></span>
+                        <span class="qq-edit-filename-icon-selector qq-btn qq-edit-filename-icon" aria-label="Edit filename"></span>
+                    </div>
+                    <input class="qq-edit-filename-selector qq-edit-filename" tabindex="0" type="text">
+                    <span class="qq-upload-size-selector qq-upload-size"></span>
+                    <button type="button" class="qq-btn qq-upload-delete-selector qq-upload-delete">
+                        <span class="qq-btn qq-delete-icon" aria-label="Delete"></span>
+                    </button>
+                    <button type="button" class="qq-btn qq-upload-pause-selector qq-upload-pause">
+                        <span class="qq-btn qq-pause-icon" aria-label="Pause"></span>
+                    </button>
+                    <button type="button" class="qq-btn qq-upload-continue-selector qq-upload-continue">
+                        <span class="qq-btn qq-continue-icon" aria-label="Continue"></span>
+                    </button>
+                </div>
+            </li>
+        </ul>
+
+        <dialog class="qq-alert-dialog-selector">
+            <div class="qq-dialog-message-selector"></div>
+            <div class="qq-dialog-buttons">
+                <button type="button" class="qq-cancel-button-selector">Close</button>
+            </div>
+        </dialog>
+
+        <dialog class="qq-confirm-dialog-selector">
+            <div class="qq-dialog-message-selector"></div>
+            <div class="qq-dialog-buttons">
+                <button type="button" class="qq-cancel-button-selector">No</button>
+                <button type="button" class="qq-ok-button-selector">Yes</button>
+            </div>
+        </dialog>
+
+        <dialog class="qq-prompt-dialog-selector">
+            <div class="qq-dialog-message-selector"></div>
+            <input type="text">
+            <div class="qq-dialog-buttons">
+                <button type="button" class="qq-cancel-button-selector">Cancel</button>
+                <button type="button" class="qq-ok-button-selector">Ok</button>
+            </div>
+        </dialog>
+    </div>
+    </script>
+            
+        </head>
 	<body>
 		<?php include 'header.php';?>
 		<main>
@@ -158,31 +246,27 @@
 									</div>
 									<div class="well">
 										<div class="well-header">
-											<h6>Especificaciones de calidad</h6>
-											<div class="well-add">
-												<a href="#"> 
-													<span class="fa fa-plus"></span>
-												</a>
-											</div>
+                                                                                        <h6>Especificaciones de calidad</h6>
 										</div>
-										<div class="well-body">
-											<div class="row">
-												<div class="col-lg-11">
-													<div class="input-group">
-														<input id="contrato-SAP" type="text" required name="contrato-SAP">
-														<span class="fa fa-file"></span>
-														<label for="contrato-SAP">Adjuntar archivo</label>
-													</div>
-												</div>
-												<div class="col-lg-1">
-													<div class="well-trash">
-														<a href="#"> 
-															<span class="fa fa-trash"></span>
-														</a>
-													</div>
-												</div>
-											</div>
-										</div>
+                                                                                <div id="contrato-SAP-upload" name="contrato-SAP-upload" class="well-body"></div>
+                                                                                <script>
+        var galleryUploader = new qq.FineUploader({
+            element: document.getElementById("contrato-SAP-upload"),
+            template: 'qq-template-gallery',
+            request: {
+                endpoint: '/server/uploads'
+            },
+            thumbnails: {
+                placeholders: {
+                    waitingPath: '/source/placeholders/waiting-generic.png',
+                    notAvailablePath: '/source/placeholders/not_available-generic.png'
+                }
+            },
+            validation: {
+                allowedExtensions: ['jpeg', 'jpg', 'gif', 'png']
+            }
+        });
+                                                                                </script>
 									</div>
 									<div class="row">
 										<div class="col-lg-12">
